d672dd557db9a026852318f1b31a7642993ab66e
ViridityMoon <viriditymoon@gmail.com>
2020-07-24 11:15:26 -0400

    Built out axiosWithAuth module and login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,47 @@
 import React from "react";
+import { axiosWithAuth } from '../auth/axiosWithAuth';
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+  const [creds, setCreds] = useState({
+    username: '',
+    password: ''
+  });
+
+  const login = e => {
+    e.preventDefault();
+    axiosWithAuth()
+        .post('/login', creds)
+        .then(res => {
+            localStorage.setItem('token', res.data.payload);
+            props.history.push('/protected');
+        })
+        .catch(err => console.log('Error: ', err.error));
+  };
+
+  const handleChange = e => {
+    setCreds({
+    ...creds,
+    [e.target.name]: e.target.value,
+    })
+  };
+
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={login}>
+          <input
+            type="text"
+            name="username"
+            value={creds.username}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={creds.password}
+            onChange={handleChange}
+          />
+          <button type='submit'>Log in</button>
+        </form>
     </>
   );
 };
